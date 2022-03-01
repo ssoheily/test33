d5b6875d02c801135f5416bac5e53ada33400de0
ssoheily <sam.soheil55@gmail.com>
2022-03-01 09:41:12 +0100

    first committ

---
+++ script.js
@@ -0,0 +1,71 @@
+// let $=document;
+
+// function saveinLocalStorage(){
+//    localStorage.setItem("id",2);
+// }
+// function test(){
+//     console.log(arguments);
+// }
+// test();/* create same array[] */
+// test(3,3);/* create same array[3,3]  */
+
+
+// function mal() {
+//     let resault = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//       resault += arguments[i];
+//     }
+//     return resault;
+//   }
+//   console.log(mal(43,34,));
+
+
+// function myfunc(...everyName){
+//     console.log(everyName);
+// }
+// myfunc();///* create array  */
+// myfunc(2,4,6,1);/* create array [2,4,6,1] */
+
+
+
+
+// function mal(...everynames) {
+//   let resault = 0;
+//   for (let i = 0; i < everynames.length; i++) {
+//     resault += everynames[i];
+//   }
+//   return resault;
+// }
+// console.log(mal(43,34,22));
+
+// function FUNCFIRST (FUNCTION_AS_PARAMETER) 
+// { 
+//     console.log("CALLBACK FUNCTION1");
+// FUNCTION_AS_PARAMETER() ; 
+// } 
+
+// function FUNCTION_AS_PARAMETER() {
+// console.log("CALLBACK FUNCTION2");
+// }
+// FUNCFIRST(FUNCTION_AS_PARAMETER);
+
+// function block_desribee(){
+//     if(true){
+//         var a=2;
+//     }
+//     console.log(a);
+// }
+// block_desribee()
+
+
+// function block_desribee(){
+//     if(true){
+//         let a=2;
+//     }
+//     console.log(a);
+// }
+// block_desribee()
+
+/* hoistin id just for declare variabel and not for insialize variabel */
+console.log(k);
+ var k=4; //undefine
